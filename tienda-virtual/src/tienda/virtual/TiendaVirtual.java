8242abcc8b7f069d667a1afbc6f1a60088a26d6e
Sebas <sebasllorente14@gmail.com>
2026-04-19 19:44:58 -0500

    Organización inicial del repositorio y actualización del readme

---
+++ tienda-virtual/src/tienda/virtual/TiendaVirtual.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tienda.virtual;
+
+/**
+ *
+ * @author Laptop HP 255 G10
+ */
+public class TiendaVirtual {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
